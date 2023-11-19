53b449bfec8e3940abfe8b7d666c327b3541edf0
Yu Zhang <zhany708@mcmaster.ca>
2023-11-19 17:02:29 -0500

    Lteration 0 done

--- Project.cpp
+++ Project.cpp
@@ -2,7 +2,6 @@
 #include "MacUILib.h"
 #include "objPos.h"
 
-
 using namespace std;
 
 #define DELAY_CONST 100000
@@ -56,7 +55,51 @@ void RunLogic(void)
 
 void DrawScreen(void)
 {
+    int i, j;
+    char symbol = '#';
+    objPos board;
+    objPos space;
+    objPos newline;
+
+
     MacUILib_clearScreen();    
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 20; j++)
+        {
+            if (i == 0 || i == 9)
+            {
+                board.setObjPos(j, i, symbol);
+                MacUILib_printf("%c", board.symbol);
+                if (j == 19)
+                {
+                    newline.setObjPos(j, i, '\n');
+                    MacUILib_printf("%c", newline.symbol);
+                }
+            }
+
+
+            else
+            {
+                if (j ==0)
+                {
+                    MacUILib_printf("%c", board.symbol);
+                }
+
+                else if (j == 19)
+                {
+                    MacUILib_printf("%c%c", board.symbol, newline.symbol);
+                }
+
+                else
+                {
+                    space.setObjPos(j, i, ' ');
+                    MacUILib_printf("%c", space.symbol);
+                }
+            }
+        }
+    }
 
 }
 
